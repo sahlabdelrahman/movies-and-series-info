0b0acd7defcb9d37351d62816dacb4c64e59c976
sahl abdelrahman <bnabdelrahman07@gmail.com>
2020-03-17 01:25:10 +0300

    Updates

---
+++ precache-manifest.5478b26c6926be7be9818c237a094cb4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb7fab4431995ab07347400932fa96c",
+    "url": "/movies-and-series-info/index.html"
+  },
+  {
+    "revision": "2293e2b42ed557e8d814",
+    "url": "/movies-and-series-info/static/css/2.c0ead9d2.chunk.css"
+  },
+  {
+    "revision": "874d6a6d82c4103bc08a",
+    "url": "/movies-and-series-info/static/css/main.c79d75ab.chunk.css"
+  },
+  {
+    "revision": "2293e2b42ed557e8d814",
+    "url": "/movies-and-series-info/static/js/2.aaeb8c35.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/movies-and-series-info/static/js/2.aaeb8c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874d6a6d82c4103bc08a",
+    "url": "/movies-and-series-info/static/js/main.066ccb34.chunk.js"
+  },
+  {
+    "revision": "cb446d6c6284db232bbf",
+    "url": "/movies-and-series-info/static/js/runtime-main.eb87d4b4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movies-and-series-info/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movies-and-series-info/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movies-and-series-info/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movies-and-series-info/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movies-and-series-info/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bd0201f17c0ee3d5efdc1f52c5466fc5",
+    "url": "/movies-and-series-info/static/media/spinner.bd0201f1.gif"
+  }
+]);
